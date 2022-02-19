8e9ca8e18a1cfab687d3cd44b5364b0ee0901c57
Tien Do Thanh <dothanhtien95@gmail.com>
2022-02-19 17:14:18 +0700

    add routes HOC

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+// routes
+import { RequireAuth, RestrictLoggedInUser } from "./HOCs/routes";
+
 // authentication pages
 import SignIn from "./views/Auth/SignIn";
 import SignUp from "./views/Auth/SignUp";
@@ -14,9 +17,34 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/sign-in" />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/projects" element={<ManageProjects />} />
+
+        <Route
+          path="/sign-in"
+          element={
+            <RestrictLoggedInUser redirectPath="/projects">
+              <SignIn />
+            </RestrictLoggedInUser>
+          }
+        />
+        
+        <Route
+          path="/sign-up"
+          element={
+            <RestrictLoggedInUser redirectPath="/projects">
+              <SignUp />
+            </RestrictLoggedInUser>
+          }
+        />
+
+        <Route
+          path="/projects"
+          element={
+            <RequireAuth redirectPath="/sign-in">
+              <ManageProjects />
+            </RequireAuth>
+          }
+        />
+
         <Route path="*" element={<Navigate to="/sign-in" />} />
       </Routes>
     </BrowserRouter>
